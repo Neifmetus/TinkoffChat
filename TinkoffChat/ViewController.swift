c8bf5aaf3a761c2ba1e8ff4b0c74a70a8e86c2d1
e.a.morozova <e.a.morozova@macmini.local>
2017-10-01 18:57:53 +0300

    Second homework with constrains and image load

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -8,11 +8,88 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var uploadPhotoView: UIView?
+    @IBOutlet weak var photoImageView: UIImageView?
+    @IBOutlet weak var editButton: UIButton?
+    @IBOutlet weak var uploadImageButton: UIButton?
+    
+    @IBAction func selectImage(_ sender: Any) {
+        let camera = CameraHandler(delegate_: self)
+        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        optionMenu.popoverPresentationController?.sourceView = self.view
 
+        let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { (alert : UIAlertAction!) in
+            camera.getPhotoViaCamera(self, canEdit: true)
+        }
+        let sharePhoto = UIAlertAction(title: "Photo Library", style: .default) { (alert : UIAlertAction!) in
+            camera.getPhotoViaLibrary(self, canEdit: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (alert : UIAlertAction!) in
+        }
+
+        optionMenu.addAction(takePhoto)
+        optionMenu.addAction(sharePhoto)
+        optionMenu.addAction(cancelAction)
+        self.present(optionMenu, animated: true, completion: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        // Фрейм не выводится, поскольку фактически инициализация элементов ViewController происходит
+        // во viewDidLoad, а init вызывается раньше
+        if let editButton = editButton {
+            print("Edit button frame in init: \(editButton.frame)")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        photoImageView?.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let uploadPhotoView = uploadPhotoView else {
+            print("lalala")
+            return
+        }
+        
+        uploadPhotoView.contentMode = .scaleAspectFill
+        uploadPhotoView.layer.cornerRadius = uploadPhotoView.frame.width / 2
+        uploadPhotoView.layer.masksToBounds = true
+
+        guard let photoImageView = photoImageView else {
+            print("lalala")
+            return
+        }
+        
+        photoImageView.layer.cornerRadius = uploadPhotoView.layer.cornerRadius
+        photoImageView.layer.masksToBounds = true
+        
+        guard let editButton = editButton else {
+            print("lalala")
+            return
+        }
+        
+        editButton.layer.borderWidth = 2
+        editButton.layer.borderColor = UIColor.black.cgColor
+        editButton.layer.cornerRadius = editButton.frame.height / 2
+        editButton.layer.masksToBounds = true
+        
+        guard let uploadImageButton = uploadImageButton else {
+            print("lalala")
+            return
+        }
+        
+        let buttonImage = UIImage(named: "slr-camera-2-xxl.png")?.withRenderingMode(.alwaysTemplate)
+        uploadImageButton.setImage(buttonImage, for: UIControlState.normal)
+        uploadImageButton.tintColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,11 +100,25 @@ class ProfileViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print(#function)
+        
+        if let editButton = editButton {
+            print("Edit button frame \(editButton.frame)" )
+        } else {
+            print("Unable to print Edit button's frame")
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print(#function)
+        
+        if let editButton = editButton {
+            print("Edit button frame \(editButton.frame)" )
+        } else {
+            print("Unable to print Edit button's frame")
+            
+            // Разница во фрейма связана с тем, что autolayout'у требуется время для калькулирования фреймов
+        }
     }
     
     override func viewWillLayoutSubviews() {
